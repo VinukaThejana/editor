cbee3285fc83bc741f34ac724458b716104992b1
Vinuka Kodituwakku <vinuka.t@pm.me>
2023-12-15 00:26:20 +0530

    Updated the lua files

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -11,18 +11,58 @@ return {
         lua_ls = {},
         eslint = {},
         tailwindcss = {},
-      },
-    },
-  },
+        clangd = {},
+        dockerls = {},
+        docker_compose_language_service = {},
 
-  {
-    "neovim/nvim-lspconfig",
-    opts = {
-      servers = {
+        ---@type lspconfig.options.tsserver
         tsserver = {
           keys = {
-            { "<leader>co", "<cmd>TypescriptOrganizeImports<CR>", desc = "Organize Imports" },
-            { "<leader>cR", "<cmd>TypescriptRenameFile<CR>", desc = "Rename File" },
+            {
+              "<leader>co",
+              function()
+                vim.lsp.buf.code_action({
+                  apply = true,
+                  context = {
+                    only = { "source.organizeImports.ts" },
+                    diagnostics = {},
+                  },
+                })
+              end,
+              desc = "Organize Imports",
+            },
+            {
+              "<leader>cR",
+              function()
+                vim.lsp.buf.code_action({
+                  apply = true,
+                  context = {
+                    only = { "source.removeUnused.ts" },
+                    diagnostics = {},
+                  },
+                })
+              end,
+              desc = "Remove Unused Imports",
+            },
+          },
+          settings = {
+            typescript = {
+              format = {
+                indentSize = vim.o.shiftwidth,
+                convertTabsToSpaces = vim.o.expandtab,
+                tabSize = vim.o.tabstop,
+              },
+            },
+            javascript = {
+              format = {
+                indentSize = vim.o.shiftwidth,
+                convertTabsToSpaces = vim.o.expandtab,
+                tabSize = vim.o.tabstop,
+              },
+            },
+            completions = {
+              completeFunctionCalls = true,
+            },
           },
         },
       },
