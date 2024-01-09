170e4cf660bdbeb3e5a8f62b83e4359e5772d575
Vinuka Kodituwakku <vinuka.t@pm.me>
2024-01-09 09:33:08 +0530

    Added tmux session manager

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -65,4 +65,21 @@ return {
       end,
     },
   },
+
+  -- Tmux sessionizer
+  {
+    "sanathks/workspace.nvim",
+    dependencies = { "nvim-telescope/telescope.nvim" },
+    config = function()
+      local workspace = require("workspace")
+      vim.keymap.set("n", "<leader><space>l", workspace.tmux_sessions)
+      require("workspace").setup({
+        workspaces = {
+          { name = "Personal", path = "~/projects/vinuka/", keymap = { "<leader><space>p" } },
+          { name = "BBI", path = "~/projects/bakesbyish/", keymap = { "<leader><space>b" } },
+          { name = "Reference", path = "~/projects/Downloads", keymap = { "<leader><space>r" } },
+        },
+      })
+    end,
+  },
 }
