01a7e72dc6b60feb8e6672fa2785986fc87db06f
Vinuka Kodituwakku <vinuka.t@pm.me>
2024-08-29 22:50:39 +0530

    feat(pairs): added auto pairs

--- lua/custom/lazy/init.lua
+++ lua/custom/lazy/init.lua
@@ -6,4 +6,53 @@ return {
 	},
 
 	"eandrju/cellular-automaton.nvim",
+
+	{
+		"echasnovski/mini.pairs",
+		config = function()
+			require("mini.pairs").setup({
+				-- Configure mappings for auto-pairing
+				mappings = {
+					["("] = { action = "open", pair = "()", neigh_pattern = "[^\\]." },
+					["["] = { action = "open", pair = "[]", neigh_pattern = "[^\\]." },
+					["{"] = { action = "open", pair = "{}", neigh_pattern = "[^\\]." },
+					[")"] = { action = "close", pair = "()", neigh_pattern = "[^\\]." },
+					["]"] = { action = "close", pair = "[]", neigh_pattern = "[^\\]." },
+					["}"] = { action = "close", pair = "{}", neigh_pattern = "[^\\]." },
+					['"'] = { action = "closeopen", pair = '""', neigh_pattern = "[^\\].", register = { cr = false } },
+					["'"] = { action = "closeopen", pair = "''", neigh_pattern = "[^%a\\].", register = { cr = false } },
+					["`"] = { action = "closeopen", pair = "``", neigh_pattern = "[^\\].", register = { cr = false } },
+				},
+				-- Configure modes where auto-pairing is active
+				modes = {
+					insert = true,
+					command = true,
+					terminal = false,
+				},
+				-- Skip autopair when next character is one of these
+				skip_next = [=[[%w%%%'%[%"%.%`%$]]=],
+				-- Skip autopair when the cursor is inside these treesitter nodes
+				skip_ts = { "string" },
+				-- Skip autopair when next character is closing pair
+				-- and there are more closing pairs than opening pairs
+				skip_unbalanced = true,
+				-- Better deal with markdown code blocks
+				markdown = true,
+				-- Disable for specific filetypes
+				disable_filetype = { "TelescopePrompt" },
+			})
+		end,
+	},
+	{
+		"folke/ts-comments.nvim",
+		event = "VeryLazy",
+		opts = {},
+	},
+	{
+		"JoosepAlviste/nvim-ts-context-commentstring",
+		lazy = true,
+		opts = {
+			enable_autocmd = false,
+		},
+	},
 }
