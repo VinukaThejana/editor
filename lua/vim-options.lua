102742a5bd30cd6ded80bf1d0e9d5550184ef7e5
Vinuka Kodituwakku <vinuka.t@pm.me>
2025-01-11 23:29:44 +0530

    fix(copilot): changed the accept key of copilot

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -105,3 +105,7 @@ vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><
 
 -- make the opened file executable
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
+
+-- Copilot change the accept key to <C-CR>
+vim.g.copilot_no_tab_map = true
+vim.api.nvim_set_keymap("i", "<C-CR>", 'copilot#Accept("<CR>")', { expr = true, silent = true })
